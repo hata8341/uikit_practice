750f96e42cf580d5442443a71370d059c23b2eca
hata <wmf8341@gmail.com>
2022-10-01 16:37:27 +0900

    simple_stack_view-commit

--- uikit_practice/ViewController.swift
+++ uikit_practice/ViewController.swift
@@ -9,12 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Root_stack_view: UIStackView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
     }
-
+    
+//    Root Stack View.Leading = Superview.LeadingMargin
+//
+//    Root Stack View.Trailing = Superview.TrailingMargin
+//
+//    Root Stack View.Top = Top Layout Guide.Bottom + 20.0
+//
+//    Bottom Layout Guide.Top = Root Stack View.Bottom + 20.0
+//
+//    Image View.Height = Image View.Width
+//
+//    First Name Text Field.Width = Middle Name Text Field.Width
+//
+//    First Name Text Field.Width = Last Name Text Field.Width
     
 
 }
