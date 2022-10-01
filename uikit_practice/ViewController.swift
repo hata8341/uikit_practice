0599ceca6194f9f12026ed2f3a62c408a4926013
hata <wmf8341@gmail.com>
2022-10-01 13:34:16 +0900

    stack_view-commit

--- uikit_practice/ViewController.swift
+++ uikit_practice/ViewController.swift
@@ -9,25 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var label_t: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        func Log(){
-            print("テスト３")
-        }
     }
 
-    @IBOutlet weak var label: UILabel!
     
-    @IBAction func sayhello(_ sender: Any) {
-        label.text = "こんにちは"
-    }
-    @IBAction func toggle_swtch(_ sender: Any) {
-    }
-    @IBAction func button_bule(_ sender: Any) {
-    }
-    @IBOutlet weak var color_ball: UIColorWell!
+
 }
 
