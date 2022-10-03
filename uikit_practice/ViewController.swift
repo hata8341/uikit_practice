45d0e787b1421a1fdc522c5ead70a304a84244e5
hata <wmf8341@gmail.com>
2022-10-03 17:17:08 +0900

    Two_Different-Width_Views-commit

--- uikit_practice/ViewController.swift
+++ uikit_practice/ViewController.swift
@@ -10,46 +10,59 @@ import UIKit
 class ViewController : UIViewController{
     
     
-    private let purpleView : UIView = UIView()
+    private let blueView : UIView = UIView()
     
-    private let orangeView : UIView = UIView()
+    private let redView : UIView = UIView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         print("スタート")
-        purpleView.backgroundColor = .purple
-        orangeView.backgroundColor = .orange
+        blueView.backgroundColor = .blue
+        redView.backgroundColor = .red
         //        AutoresizingMask を 無効化 Auto Layout変換
         //        newView.backgroundColor = .blue
-        purpleView.translatesAutoresizingMaskIntoConstraints = false
-        orangeView.translatesAutoresizingMaskIntoConstraints = false
+        blueView.translatesAutoresizingMaskIntoConstraints = false
+        redView.translatesAutoresizingMaskIntoConstraints = false
         
-        self.view.addSubview(purpleView)
-        self.view.addSubview(orangeView)
+        self.view.addSubview(blueView)
+        self.view.addSubview(redView)
         
-        let margin = self.view.layoutMarginsGuide
-//        Purple View.Leading = Superview.LeadingMargin
-        let purpleLead = purpleView.leadingAnchor.constraint(equalTo: margin.leadingAnchor)
-//        Orange View.Leading = Purple View.Trailing + Standard
-        let orangeLead = orangeView.leadingAnchor.constraint(equalToSystemSpacingAfter: purpleView.trailingAnchor, multiplier: 1.0)
+        
 //
-//        Orange View.Trailing = Superview.TrailingMargin
-        let orangeTrail = orangeView.trailingAnchor.constraint(equalTo: margin.trailingAnchor)
-//        Purple View.Top = Top Layout Guide.Bottom + 20.0
-        let purpleTop = purpleView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 20.0)
-//        Orange View.Top = Top Layout Guide.Bottom + 20.0
-        let orangeTop = orangeView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 20.0)
-//        Bottom Layout Guide.Top = Purple View.Bottom + 20.0
-        let purplebottom = self.view.safeAreaLayoutGuide.bottomAnchor.constraint(equalTo: purpleView.bottomAnchor, constant: 20.0)
-//        Bottom Layout Guide.Top = Orange View.Bottom + 20.0
-        let orangeBottom = self.view.safeAreaLayoutGuide.bottomAnchor.constraint(equalTo: orangeView.bottomAnchor, constant: 20.0)
-//        Orange View.Width = 2.0 x Purple View.Width
-        let orangeWidth = orangeView.widthAnchor.constraint(equalTo: purpleView.widthAnchor, multiplier: 2.0)
+//
+//
+//
+//
+//
+//
+//
+
 
         
+        let margin = self.view.layoutMarginsGuide
+    //   Blue View.Leading = Superview.LeadingMargin
+        let blueLead = blueView.leadingAnchor.constraint(equalTo: margin.leadingAnchor)
+    //        Red View.Leading = Blue View.Trailing + Standard
+        let redLead = redView.leadingAnchor.constraint(equalToSystemSpacingAfter: blueView.trailingAnchor, multiplier: 1.0)        //        Red View.Trailing = Superview.TrailingMargin
+        let redTrail = redView.trailingAnchor.constraint(equalTo: margin.trailingAnchor)
+//        Blue View.Top = Top Layout Guide.Bottom + 20.0
+        let blueTop = blueView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 20.0)
+//        Red View.Top = Top Layout Guide.Bottom + 20.0
+        let redTop = redView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 20.0)
+//        Bottom Layout Guide.Top = Blue View.Bottom + 20.0
+        let bluebottom = self.view.safeAreaLayoutGuide.bottomAnchor.constraint(equalTo: blueView.bottomAnchor, constant: 20.0)
+//        Bottom Layout Guide.Top = Red View.Bottom + 20.0
+        let redBottom = self.view.safeAreaLayoutGuide.bottomAnchor.constraint(equalTo: redView.bottomAnchor, constant: 20.0)
+//        Red View.Width = 2.0 x Blue View.Width (Priority 750)
+        let redWidth = redView.widthAnchor.constraint(equalTo: blueView.widthAnchor, multiplier: 2.0)
+        blueView.setContentHuggingPriority(UILayoutPriority(750), for: .horizontal)
+
+        
+        //        Blue View.Width >= 150.0
+        let blueWidth = blueView.widthAnchor.constraint(greaterThanOrEqualToConstant: 150.0)
         //制約を有効化します
 //        NSLayoutConstraint.activate([yellowLead,greenLead,greenTrail,yellowTop,greenTop,yellowBottom,greenBottom,yellowGreenWidth])
-        NSLayoutConstraint.activate([purpleLead,orangeLead,orangeTrail,purpleTop,orangeTop,purplebottom,orangeBottom,orangeWidth])
+        NSLayoutConstraint.activate([blueLead,redLead,redTrail,blueTop,redTop,bluebottom,redBottom,redWidth,blueWidth])
         
     }
     
