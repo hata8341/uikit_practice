4dc64cdda78872314c7a2d5250feda06936bdee3
hata <wmf8341@gmail.com>
2022-10-03 16:25:37 +0900

    Two_Different-Width_Views-commit

--- uikit_practice/ViewController.swift
+++ uikit_practice/ViewController.swift
@@ -9,57 +9,47 @@ import UIKit
 
 class ViewController : UIViewController{
     
-    //
-    //
-    //
-    //
-    //
-    //
-    //
     
+    private let purpleView : UIView = UIView()
     
-    private let yellowView : UIView = UIView()
-    
-    private let greenView : UIView = UIView()
+    private let orangeView : UIView = UIView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         print("スタート")
-        yellowView.backgroundColor = .yellow
-        greenView.backgroundColor = .green
+        purpleView.backgroundColor = .purple
+        orangeView.backgroundColor = .orange
         //        AutoresizingMask を 無効化 Auto Layout変換
         //        newView.backgroundColor = .blue
-        yellowView.translatesAutoresizingMaskIntoConstraints = false
-        greenView.translatesAutoresizingMaskIntoConstraints = false
+        purpleView.translatesAutoresizingMaskIntoConstraints = false
+        orangeView.translatesAutoresizingMaskIntoConstraints = false
         
-        self.view.addSubview(yellowView)
-        self.view.addSubview(greenView)
-        //
-        //        newView.translatesAutoresizingMaskIntoConstraints = false
-        //
-        //        view.addSubview(newView)
+        self.view.addSubview(purpleView)
+        self.view.addSubview(orangeView)
         
-        let margin = view.layoutMarginsGuide
-        //    Yellow View.Leading = Superview.LeadingMargin
-        let yellowLead = yellowView.leadingAnchor.constraint(equalTo: margin.leadingAnchor)
-        //    Green View.Leading = Yellow View.Trailing + Standard
-        let greenLead = greenView.leadingAnchor.constraint(equalToSystemSpacingAfter: yellowView.trailingAnchor, multiplier: 1.0)
-        //    Green View.Trailing = Superview.TrailingMargin
-        let greenTrail = greenView.trailingAnchor.constraint(equalTo: margin.trailingAnchor)
-        //    Yellow View.Top = Top Layout Guide.Bottom + 20.0
-        let yellowTop = yellowView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 20.0)
-        //    Green View.Top = Top Layout Guide.Bottom + 20.0
-        let greenTop = greenView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 20.0)
-        //    Bottom Layout Guide.Top = Yellow View.Bottom + 20.0
-        let yellowBottom =
-        self.view.safeAreaLayoutGuide.bottomAnchor.constraint(equalTo: yellowView.bottomAnchor, constant: 20.0)
-        //    Bottom Layout Guide.Top = Green View.Bottom + 20.0
-        let greenBottom =
-        self.view.safeAreaLayoutGuide.bottomAnchor.constraint(equalTo: greenView.bottomAnchor, constant: 20.0)
-        //    Yellow View.Width = Green View.Width
-        let yellowGreenWidth = yellowView.widthAnchor.constraint(equalTo: greenView.widthAnchor)
+        let margin = self.view.layoutMarginsGuide
+//        Purple View.Leading = Superview.LeadingMargin
+        let purpleLead = purpleView.leadingAnchor.constraint(equalTo: margin.leadingAnchor)
+//        Orange View.Leading = Purple View.Trailing + Standard
+        let orangeLead = orangeView.leadingAnchor.constraint(equalToSystemSpacingAfter: purpleView.trailingAnchor, multiplier: 1.0)
+//
+//        Orange View.Trailing = Superview.TrailingMargin
+        let orangeTrail = orangeView.trailingAnchor.constraint(equalTo: margin.trailingAnchor)
+//        Purple View.Top = Top Layout Guide.Bottom + 20.0
+        let purpleTop = purpleView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 20.0)
+//        Orange View.Top = Top Layout Guide.Bottom + 20.0
+        let orangeTop = orangeView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 20.0)
+//        Bottom Layout Guide.Top = Purple View.Bottom + 20.0
+        let purplebottom = self.view.safeAreaLayoutGuide.bottomAnchor.constraint(equalTo: purpleView.bottomAnchor, constant: 20.0)
+//        Bottom Layout Guide.Top = Orange View.Bottom + 20.0
+        let orangeBottom = self.view.safeAreaLayoutGuide.bottomAnchor.constraint(equalTo: orangeView.bottomAnchor, constant: 20.0)
+//        Orange View.Width = 2.0 x Purple View.Width
+        let orangeWidth = orangeView.widthAnchor.constraint(equalTo: purpleView.widthAnchor, multiplier: 2.0)
+
+        
         //制約を有効化します
-        NSLayoutConstraint.activate([yellowLead,greenLead,greenTrail,yellowTop,greenTop,yellowBottom,greenBottom,yellowGreenWidth])
+//        NSLayoutConstraint.activate([yellowLead,greenLead,greenTrail,yellowTop,greenTop,yellowBottom,greenBottom,yellowGreenWidth])
+        NSLayoutConstraint.activate([purpleLead,orangeLead,orangeTrail,purpleTop,orangeTop,purplebottom,orangeBottom,orangeWidth])
         
     }
     
