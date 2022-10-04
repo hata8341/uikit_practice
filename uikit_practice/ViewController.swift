24dddbfd33c64341e27a86e668f3ea419df7163a
hata <wmf8341@gmail.com>
2022-10-04 13:21:07 +0900

    Fixed_Height_Columns-commit

--- uikit_practice/ViewController.swift
+++ uikit_practice/ViewController.swift
@@ -10,68 +10,158 @@ import UIKit
 class ViewController : UIViewController{
     
     private let lableName : UILabel = UILabel()
+    private let midLableName : UILabel = UILabel()
+    private let lastLableName : UILabel = UILabel()
     
     private let textName : UITextField = UITextField()
+    private let midTextName : UITextField = UITextField()
+    private let lastTextName : UITextField = UITextField()
+    
+    
     
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         print("スタート")
-//        AutoresizingMask を 無効化 Auto Layout変換
+        //        AutoresizingMask を 無効化 Auto Layout変換
         lableName.translatesAutoresizingMaskIntoConstraints = false
         textName.translatesAutoresizingMaskIntoConstraints = false
         
-        lableName.text = "Name"
+        midLableName.translatesAutoresizingMaskIntoConstraints = false
+        lastLableName.translatesAutoresizingMaskIntoConstraints = false
         
-        textName.placeholder = "Enter name"
+        midTextName.translatesAutoresizingMaskIntoConstraints = false
+        lastTextName.translatesAutoresizingMaskIntoConstraints = false
+        
+        lableName.text = "First Name"
+        midLableName.text = "Middle Name"
+        lastLableName.text = "Last Name"
+        
+        lableName.textAlignment = .right
+        midLableName.textAlignment = .right
+        lastLableName.textAlignment = .right
+        
+        textName.placeholder = "Enter first name"
         textName.layer.borderColor = UIColor.lightGray.cgColor
         textName.layer.borderWidth = 1
         textName.borderStyle = .roundedRect
         textName.layer.cornerRadius = 5
         
+        midTextName.placeholder = "Enter middle name"
+        midTextName.layer.borderColor = UIColor.lightGray.cgColor
+        midTextName.layer.borderWidth = 1
+        midTextName.borderStyle = .roundedRect
+        midTextName.layer.cornerRadius = 5
+        
+        lastTextName.placeholder = "Enter last name"
+        lastTextName.layer.borderColor = UIColor.lightGray.cgColor
+        lastTextName.layer.borderWidth = 1
+        lastTextName.borderStyle = .roundedRect
+        lastTextName.layer.cornerRadius = 5
+        
         view.addSubview(lableName)
         view.addSubview(textName)
         
+        view.addSubview(midLableName)
+        view.addSubview(midTextName)
+        
+        view.addSubview(lastLableName)
+        view.addSubview(lastTextName)
+        
         let margin = view.layoutMarginsGuide
         
-//        Name Label.Leading = Superview.LeadingMargin
-        let lableLeading = lableName.leadingAnchor.constraint(equalTo: margin.leadingAnchor)
-//        Name Text Field.Trailing = Superview.TrailingMargin
-        let textNameTrail = textName.trailingAnchor.constraint(equalTo: margin.trailingAnchor)
-//        print(textName.layoutMarginsGuide.)
-//        standard spaceの取得の仕方から
-//        ビューとそのスーパービューの端の間は20.0ポイント、兄弟ビューの間は8.0ポイントの間隔が標準となっています
-//        Name Text Field.Leading = Name Label.Trailing + Standard
-        let textNameLead = textName.leadingAnchor.constraint(equalTo: lableName.trailingAnchor, constant: 8.0)
-//        Name Label.Top >= Top Layout Guide.Bottom + 20.0
-        let labelTopGreater = lableName.topAnchor.constraint(greaterThanOrEqualTo: view.safeAreaLayoutGuide.topAnchor, constant: 20.0)
-//        Name Label.Top = Top Layout Guide.Bottom + 20.0 (Priority 249)
-        let labelTopPri = lableName.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20.0)
-        labelTopPri.priority = UILayoutPriority(249)
+        //        First Name Label.Leading = Superview.LeadingMargin
+        let firstLabelLead = lableName.leadingAnchor.constraint(equalTo: margin.leadingAnchor)
+        //        Middle Name Label.Leading = Superview.LeadingMargin
+        let midLabelLead = midLableName.leadingAnchor.constraint(equalTo: margin.leadingAnchor)
+        //        Last Name Label.Leading = Superview.LeadingMargin
+        let lastLabelLead = lastLableName.leadingAnchor.constraint(equalTo: margin.leadingAnchor)
+        //        First Name Text Field.Leading = First Name Label.Trailing + Standard
+        let firstTextLead = textName.leadingAnchor.constraint(equalTo: lableName.trailingAnchor, constant: 8.0)
+        //        Middle Name Text Field.Leading = Middle Name Label.Trailing + Standard
+        let midTextLead = midTextName.leadingAnchor.constraint(equalTo: midLableName.trailingAnchor, constant: 8.0)
+        //        Last Name Text Field.Leading = Last Name Label.Trailing + Standard
+        let lastTextLead = lastTextName.leadingAnchor.constraint(equalTo: lastLableName.trailingAnchor, constant: 8.0)
+        //        First Name Text Field.Trailing = Superview.TrailingMargin
+        let firstTextTrail = textName.trailingAnchor.constraint(equalTo: margin.trailingAnchor)
+        //        Middle Name Text Field.Trailing = Superview.TrailingMargin
+        let midTextTrail = midTextName.trailingAnchor.constraint(equalTo: margin.trailingAnchor)
+        //        Last Name Text Field.Trailing = Superview.TrailingMargin
+        let lastTextTrail = lastTextName.trailingAnchor.constraint(equalTo: margin.trailingAnchor)
+        //        First Name Label.Baseline = First Name Text Field.Baseline
+        let firstLableBaseline = lableName.firstBaselineAnchor.constraint(equalTo: textName.firstBaselineAnchor)
+        //        Middle Name Label.Baseline = Middle Name Text Field.Baseline
+        let midLableBaseline = midLableName.firstBaselineAnchor.constraint(equalTo: midTextName.firstBaselineAnchor)
+        //        Last Name Label.Baseline = Last Name Text Field.Baseline
+        let lastLableBaseline = lastLableName.lastBaselineAnchor.constraint(equalTo: lastTextName.lastBaselineAnchor)
+        //        First Name Text Field.Width = Middle Name Text Field.Width
+        let firstTextWidthToMid = textName.widthAnchor.constraint(equalTo: midTextName.widthAnchor)
+        //        First Name Text Field.Width = Last Name Text Field.Width
+        let firstTextWidthToLast = textName.widthAnchor.constraint(equalTo: lastTextName.widthAnchor)
+        //        First Name Text Field.Top = Top Layout Guide.Bottom + 20.0
+        let firstTextTop = textName.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor,constant: 20.0)
+        //        Middle Name Text Field.Top = First Name Text Field.Bottom + Standard
+        let midTextTop = midTextName.topAnchor.constraint(equalTo: textName.bottomAnchor,constant: 8.0)
         
-//        Name Text Field.Top >= Top Layout Guide.Bottom + 20.0
-        let textNameTopGreater = textName.topAnchor.constraint(greaterThanOrEqualTo: view.safeAreaLayoutGuide.topAnchor, constant: 20.0)
-//        Name Text Field.Top = Top Layout Guide.Bottom + 20.0 (Priority 249)
-        let textNameTopPri = textName.topAnchor.constraint(equalTo: view.topAnchor, constant: 20.0)
-        textNameTopPri.priority = UILayoutPriority(249)
-//        Name label.Baseline = Name Text Field.Baseline
-        let labelBaseline = lableName.firstBaselineAnchor.constraint(equalTo: textName.firstBaselineAnchor)
-
-
-
-//        優先度から
+        //        Last Name Text Field.Top = Middle Name Text Field.Bottom + Standard
+        let lastTextTop = lastTextName.topAnchor.constraint(equalTo: midTextName.bottomAnchor,constant: 8.0)
+        
+        
+        
+        
+        
+        //        優先度から
         lableName.setContentHuggingPriority(UILayoutPriority(251), for: .horizontal)
-        lableName.setContentHuggingPriority(.init(251), for: .vertical)
-        lableName.setContentCompressionResistancePriority(.init(750), for: .horizontal)
-        lableName.setContentCompressionResistancePriority(.init(750), for: .vertical)
-
-        textName.setContentHuggingPriority(.init(250), for: .horizontal)
-        textName.setContentHuggingPriority(.init(250), for: .vertical)
-        textName.setContentCompressionResistancePriority(.init(750), for: .horizontal)
-        textName.setContentCompressionResistancePriority(.init(750), for: .vertical)
-//        //制約を有効化します
-        NSLayoutConstraint.activate([lableLeading,textNameTrail,textNameLead,labelTopGreater,labelTopPri,textNameTopPri,textNameTopGreater,labelBaseline])
+        lableName.setContentHuggingPriority(UILayoutPriority(251), for: .vertical)
+        lableName.setContentCompressionResistancePriority(UILayoutPriority(750), for: .horizontal)
+        lableName.setContentCompressionResistancePriority(UILayoutPriority(750), for: .vertical)
+        
+        textName.setContentHuggingPriority(UILayoutPriority(250), for: .horizontal)
+        textName.setContentHuggingPriority(UILayoutPriority(250), for: .vertical)
+        textName.setContentCompressionResistancePriority(UILayoutPriority(750), for: .horizontal)
+        textName.setContentCompressionResistancePriority(UILayoutPriority(750), for: .vertical)
+        
+        midLableName.setContentHuggingPriority(UILayoutPriority(251), for: .horizontal)
+        midLableName.setContentHuggingPriority(UILayoutPriority(251), for: .vertical)
+        midLableName.setContentCompressionResistancePriority(UILayoutPriority(750), for: .horizontal)
+        midLableName.setContentCompressionResistancePriority(UILayoutPriority(750), for: .vertical)
+        
+        midTextName.setContentHuggingPriority(UILayoutPriority(250), for: .horizontal)
+        midTextName.setContentHuggingPriority(UILayoutPriority(250), for: .vertical)
+        midTextName.setContentCompressionResistancePriority(UILayoutPriority(750), for: .horizontal)
+        midTextName.setContentCompressionResistancePriority(UILayoutPriority(750), for: .vertical)
+        
+        lastLableName.setContentHuggingPriority(UILayoutPriority(251), for: .horizontal)
+        lastLableName.setContentHuggingPriority(UILayoutPriority(251), for: .vertical)
+        lastLableName.setContentCompressionResistancePriority(UILayoutPriority(750), for: .horizontal)
+        lastLableName.setContentCompressionResistancePriority(UILayoutPriority(750), for: .vertical)
+        
+        lastTextName.setContentHuggingPriority(UILayoutPriority(250), for: .horizontal)
+        lastTextName.setContentHuggingPriority(UILayoutPriority(250), for: .vertical)
+        lastTextName.setContentCompressionResistancePriority(UILayoutPriority(750), for: .horizontal)
+        lastTextName.setContentCompressionResistancePriority(UILayoutPriority(750), for: .vertical)
+        //        //制約を有効化します
+        NSLayoutConstraint.activate([
+            firstLabelLead,
+            midLabelLead,
+            lastLabelLead,
+            firstTextLead,
+            midTextLead,
+            lastTextLead,
+            firstTextTrail,
+            midTextTrail,
+            lastTextTrail,
+            firstLableBaseline,
+            midLableBaseline,
+            lastLableBaseline,
+            firstTextWidthToMid,
+            firstTextWidthToLast,
+            firstTextTop,
+            midTextTop,
+            lastTextTop
+        ])
+        print("終了")
         
     }
     
