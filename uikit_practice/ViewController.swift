f13e0daf344202b780bac8b15720934c72dc2a35
hata <wmf8341@gmail.com>
2022-10-04 09:59:00 +0900

    Simple_Label_and_Text_Field-commit

--- uikit_practice/ViewController.swift
+++ uikit_practice/ViewController.swift
@@ -9,48 +9,58 @@ import UIKit
 
 class ViewController : UIViewController{
     
+    private let lableName : UILabel = UILabel()
     
-    private let blueView : UIView = UIView()
+    private let textName : UITextField = UITextField()
     
-    private let redView : UIView = UIView()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         print("スタート")
-        blueView.backgroundColor = .blue
-        redView.backgroundColor = .red
-        //        AutoresizingMask を 無効化 Auto Layout変換
-        //        newView.backgroundColor = .blue
-        blueView.translatesAutoresizingMaskIntoConstraints = false
-        redView.translatesAutoresizingMaskIntoConstraints = false
+//        AutoresizingMask を 無効化 Auto Layout変換
+        lableName.translatesAutoresizingMaskIntoConstraints = false
+        textName.translatesAutoresizingMaskIntoConstraints = false
         
-        self.view.addSubview(blueView)
-        self.view.addSubview(redView)
+        lableName.text = "Name"
         
+        textName.placeholder = "Enter name"
+        textName.layer.borderColor = UIColor.lightGray.cgColor
+        textName.layer.borderWidth = 1
+        textName.borderStyle = .roundedRect
+        textName.layer.cornerRadius = 5
         
-        let margin = self.view.layoutMarginsGuide
-        //   Blue View.Leading = Superview.LeadingMargin
-        let blueLead = blueView.leadingAnchor.constraint(equalTo: margin.leadingAnchor)
-        //        Red View.Leading = Blue View.Trailing + Standard
-        let redLead = redView.leadingAnchor.constraint(equalToSystemSpacingAfter: blueView.trailingAnchor, multiplier: 1.0)        //        Red View.Trailing = Superview.TrailingMargin
-        let redTrail = redView.trailingAnchor.constraint(equalTo: margin.trailingAnchor)
-        //        Blue View.Top = Top Layout Guide.Bottom + 20.0
-        let blueTop = blueView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 20.0)
-        //        Red View.Top = Top Layout Guide.Bottom + 20.0
-        let redTop = redView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 20.0)
-        //        Bottom Layout Guide.Top = Blue View.Bottom + 20.0
-        let bluebottom = self.view.safeAreaLayoutGuide.bottomAnchor.constraint(equalTo: blueView.bottomAnchor, constant: 20.0)
-        //        Bottom Layout Guide.Top = Red View.Bottom + 20.0
-        let redBottom = self.view.safeAreaLayoutGuide.bottomAnchor.constraint(equalTo: redView.bottomAnchor, constant: 20.0)
-        //        Red View.Width = 2.0 x Blue View.Width (Priority 750)
-        let redWidth = redView.widthAnchor.constraint(equalTo: blueView.widthAnchor, multiplier: 2.0)
-        blueView.setContentHuggingPriority(UILayoutPriority(750), for: .horizontal)
+        view.addSubview(lableName)
+        view.addSubview(textName)
         
+        let margin = view.layoutMarginsGuide
         
-        //        Blue View.Width >= 150.0
-        let blueWidth = blueView.widthAnchor.constraint(greaterThanOrEqualToConstant: 150.0)
-        //制約を有効化します
-        NSLayoutConstraint.activate([blueLead,redLead,redTrail,blueTop,redTop,bluebottom,redBottom,redWidth,blueWidth])
+//        Name Label.Leading = Superview.LeadingMargin
+        let lableLeading = lableName.leadingAnchor.constraint(equalTo: margin.leadingAnchor)
+//        Name Text Field.Trailing = Superview.TrailingMargin
+        let textNameTrail = textName.trailingAnchor.constraint(equalTo: margin.trailingAnchor)
+//        print(textName.layoutMarginsGuide.)
+//        standard spaceの取得の仕方から
+//        ビューとそのスーパービューの端の間は20.0ポイント、兄弟ビューの間は8.0ポイントの間隔が標準となっています
+//        Name Text Field.Leading = Name Label.Trailing + Standard
+        let textNameLead = textName.leadingAnchor.constraint(equalTo: lableName.trailingAnchor, constant: 8.0)
+//        Name Text Field.Top = Top Layout Guide.Bottom + 20.0
+        let textNameTop = textName.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20.0)
+//        Name label.Baseline = Name Text Field.Baseline
+        let lableBaseline = lableName.firstBaselineAnchor.constraint(equalTo: textName.firstBaselineAnchor)
+
+//        優先度から
+        lableName.setContentHuggingPriority(UILayoutPriority(251), for: .horizontal)
+        lableName.setContentHuggingPriority(.init(251), for: .vertical)
+        lableName.setContentCompressionResistancePriority(.init(750), for: .horizontal)
+        lableName.setContentCompressionResistancePriority(.init(750), for: .vertical)
+
+        textName.setContentHuggingPriority(.init(250), for: .horizontal)
+        textName.setContentHuggingPriority(.init(250), for: .vertical)
+        textName.setContentCompressionResistancePriority(.init(750), for: .horizontal)
+        textName.setContentCompressionResistancePriority(.init(750), for: .vertical)
+//        //制約を有効化します
+        NSLayoutConstraint.activate([lableLeading,textNameTrail,textNameLead,textNameTop,lableBaseline])
         
     }
     
