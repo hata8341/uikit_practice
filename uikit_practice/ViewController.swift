98f64d621d37b4e7b4af66c5727924723bbe5057
hata <wmf8341@gmail.com>
2022-10-04 18:47:55 +0900

    Three_Equal-Width_Buttons-commit

--- uikit_practice/ViewController.swift
+++ uikit_practice/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class ViewController : UIViewController{
     
     private let shortButton : UIButton = UIButton()
+    private let midButton : UIButton = UIButton()
     private let longButton : UIButton = UIButton()
     
     
@@ -20,28 +21,38 @@ class ViewController : UIViewController{
         print("スタート")
         //        AutoresizingMask を 無効化 Auto Layout変換
         shortButton.translatesAutoresizingMaskIntoConstraints = false
+        midButton.translatesAutoresizingMaskIntoConstraints = false
         longButton.translatesAutoresizingMaskIntoConstraints = false
         shortButton.backgroundColor = .lightGray
+        midButton.backgroundColor = .lightGray
         longButton.backgroundColor = .lightGray
         
-        shortButton.setTitle("short", for: .normal)
+        shortButton.setTitle("Short", for: .normal)
+        midButton.setTitle("Medium", for: .normal)
         longButton.setTitle("Much Longer Button Title", for: .normal)
         
         view.addSubview(shortButton)
+        view.addSubview(midButton)
         view.addSubview(longButton)
         
         let margins = view.layoutMarginsGuide
         
 //        Short Button.Leading = Superview.LeadingMargin
         let shortButtonLead = shortButton.leadingAnchor.constraint(equalTo: margins.leadingAnchor)
-//        Long Button.Leading = Short Button.Trailing + Standard
-        let longButtonLead = longButton.leadingAnchor.constraint(equalTo: shortButton.trailingAnchor, constant: 8.0)
+//        Medium Button.Leading = Short Button.Trailing + Standard
+        let midButtonLead = midButton.leadingAnchor.constraint(equalTo: shortButton.trailingAnchor,constant: 8.0)
+//        Long Button.Leading = Medium Button.Trailing + Standard
+        let longButtonLead = longButton.leadingAnchor.constraint(equalTo: midButton.trailingAnchor,constant: 8.0)
 //        Long Button.Trailing = Superview.TrailingMargin
         let longButtonTrail = longButton.trailingAnchor.constraint(equalTo: margins.trailingAnchor)
 //        Bottom Layout Guide.Top = Short Button.Bottom + 20.0
-        let shortButtomBtm = view.safeAreaLayoutGuide.bottomAnchor.constraint(equalTo: shortButton.bottomAnchor, constant: 20.0)
-//        Bottom Layout Guide.Top = Long Button.Botton + 20.0
-        let longButtomBtm = view.safeAreaLayoutGuide.bottomAnchor.constraint(equalTo: longButton.bottomAnchor,constant: 20.0)
+        let shortButtonBtm = view.safeAreaLayoutGuide.bottomAnchor.constraint(equalTo: shortButton.bottomAnchor, constant: 20.0)
+//        Bottom Layout Guide.Top = Medium Button.Bottom + 20.0
+        let midButtonBtm = view.safeAreaLayoutGuide.bottomAnchor.constraint(equalTo: midButton.bottomAnchor, constant: 20.0)
+//        Bottom Layout Guide.Top = Long Button.Bottom + 20.0
+        let longButtonBtm = view.safeAreaLayoutGuide.bottomAnchor.constraint(equalTo: longButton.bottomAnchor, constant: 20.0)
+//        Short Button.Width = Medium Button.Width
+        let shortButtonWidthToMid = shortButton.widthAnchor.constraint(equalTo: midButton.widthAnchor)
 //        Short Button.Width = Long Button.Width
         let shortButtonWidthToLong = shortButton.widthAnchor.constraint(equalTo: longButton.widthAnchor)
 
@@ -52,9 +63,12 @@ class ViewController : UIViewController{
             shortButtonLead,
             longButtonLead,
             longButtonTrail,
-            shortButtomBtm,
-            longButtomBtm,
-            shortButtonWidthToLong
+            shortButtonWidthToLong,
+            midButtonLead,
+            shortButtonBtm,
+            midButtonBtm,
+            longButtonBtm,
+            shortButtonWidthToMid
         ])
         print("終了")
         
